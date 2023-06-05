4fb5595f1e6c143c6d8903f681b6d9bf47ca9918
Luigidefilippo <defilippoluigi65@gmail.com>
2023-06-05 17:15:00 +0200

    create table in index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,6 +1,30 @@
 @extends('layouts.admin')
 
 @section('content')
-<h1>I nostri progetti : </h1>
-    
+<h1>Progetti </h1>
+<table class="table">
+    <a class="btn btn-primary mt-4" href="{{ route('admin.dashboard') }}">Torna Indietro</a>
+    <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Identificativo</th>
+            <th scope="col">Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($projects as $project)
+            <tr>
+                <th scope="row">{{ $project->id }}</th>
+                <td>{{ $project->title }}</td>
+                <td>{{ $project->slug }}</td>
+                <td>
+                    <a href="{{ route('admin.projects.show', $project->slug) }}" class="btn btn-success">
+                        <i class="fa-solid fa-eye"></i>
+                    </a>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
 @endsection
