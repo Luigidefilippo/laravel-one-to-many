cb4cb32aa89201ebd4e9e1af6886b2fe0021cfb5
Luigidefilippo <defilippoluigi65@gmail.com>
2023-06-08 01:24:50 +0200

    compleate edit ma da fixare

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -8,6 +8,17 @@
         <label for="title" class="form-label">title</label>
         <input type="text" class="form-control" id="title" name="title" value="{{old('title', $project->title)}}">
     </div>
+
+    <div class="mb-3">
+        <label for="type">Select Type</label>
+        <select class="form-select" id="type" name="type_id">
+            <option selected value=""></option>
+            @foreach ($types as $type)
+                <option value="{{ $type->id }}"  @selected($type->id == old('type_id', $project->type?->id))>{{ $type->name }}</option>
+            @endforeach
+        </select>
+    </div>
+
     <div class="mb-3">
         <label for="description" class="form-label">content</label>
         <textarea class="form-control"  id="description" name='description' rows="3">{{old('description', $project->description)}}</textarea>
